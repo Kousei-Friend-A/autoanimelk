8b90aade957764fe57ede30e2ab87889c5dcf462
Friend A <101492420+Kousei-Friend-A@users.noreply.github.com>
2024-10-04 12:56:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,17 +3,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("8143727"))
-API_HASH = getenv("e2e9b22c6522465b62d8445840a526b1")
-BOT_TOKEN = getenv("7838707055:AAFQGT34pjPbjqZzo9LyaleBNDD-QpFFZII")
+API_ID = 8143727
+API_HASH = "e2e9b22c6522465b62d8445840a526b1"
+BOT_TOKEN = "7838707055:AAFQGT34pjPbjqZzo9LyaleBNDD-QpFFZII"
 
-MONGO_DB_URI = getenv("mongodb+srv://peacefulwolfdev:jR9mR92gQC1QhpdZ@cluster0.jbkwr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MONGO_DB_URI = "mongodb+srv://friendakouseimanu:nGm1RUSjMwKPC96G@cluster0.1trpq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 INDEX_ID = int(getenv("-1002440194508"))
 UPLOADS_ID = int(getenv("-1002448258068"))
 
-STATUS_ID = int(getenv("2"))
-SCHEDULE_ID = int(getenv("3"))
+STATUS_ID = 2
+SCHEDULE_ID = 3
 
 CHANNEL_TITLE = getenv("Anime Upload Main")
 INDEX_USERNAME = getenv("AnimeUploadIndex")
