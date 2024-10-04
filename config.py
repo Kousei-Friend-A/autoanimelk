245a2d9087e2ee2f27f6f0ab91f32f79049b0cbf
Friend A <101492420+Kousei-Friend-A@users.noreply.github.com>
2024-10-04 12:36:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ API_ID = int(getenv("8143727"))
 API_HASH = getenv("e2e9b22c6522465b62d8445840a526b1")
 BOT_TOKEN = getenv("7838707055:AAFQGT34pjPbjqZzo9LyaleBNDD-QpFFZII")
 
-MONGO_DB_URI = getenv("mongodb+srv://friendakouseimanu:nonamemanu911@cluster0.1trpq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MONGO_DB_URI = getenv("mongodb+srv://peacefulwolfdev:jR9mR92gQC1QhpdZ@cluster0.jbkwr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 INDEX_ID = int(getenv("-1002440194508"))
 UPLOADS_ID = int(getenv("-1002448258068"))
